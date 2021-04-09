533581c004845f26edc892675f8fb92b16847584
Pontus Laos <laos.pontus@gmail.com>
2021-04-09 11:08:05 +0200

    Cleaned up config.def.h a bit

--- config.h
+++ config.h
@@ -1,31 +1,27 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
-/* -fn option overrides fonts[0]; default X11 font or font set */
+static int topbar = 1;
+static int fuzzy  = 1;
+
 static const char *fonts[] = {
-	"Cascadia Code:size=12",
+    "Cascadia Code:size=12",
     "Noto Color Emoji:size=12"
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+static const char *prompt = NULL;
 
 static const char *colors[SchemeLast][2] = {
-	/*                         fg         bg       */
-	[SchemeNorm]          = { "#bbbbbb", "#222222" },
-	[SchemeSel]           = { "#222222", "#ffdd00" },
-	[SchemeSelHighlight]  = { "#2233FF", "#ffdd00" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
-	[SchemeOut]           = { "#000000", "#00ffff" },
+    /*                         fg         bg       */
+    [SchemeNorm]          = { "#bbbbbb", "#222222" },
+    [SchemeSel]           = { "#222222", "#ffdd00" },
+    [SchemeSelHighlight]  = { "#2233FF", "#ffdd00" },
+    [SchemeNormHighlight] = { "#ffc978", "#222222" },
+    [SchemeOut]           = { "#000000", "#00ffff" },
 };
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
-/* -h option; minimum height of a menu line */
-static unsigned int lineheight = 0;
+
+static unsigned int lines          = 0;
+static unsigned int lineheight     = 0;
 static unsigned int min_lineheight = 8;
 
-/*
- * Characters not considered part of a word while deleting words
- * for example: " /?\"&[]"
- */
 static const char worddelimiters[] = " ";
